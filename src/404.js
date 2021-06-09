47d6068b88b2b6bbd84caa476779e1c424ee6225
Olain <olain@ymp.ltd>
2021-06-09 20:09:59 +0800

    added 404 page and fixed redirect issue in index.html

---
+++ src/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+let styles = {
+    marginTop: '2em'
+};
+const NotFoundPage = () => {
+  return <h1 className="display-1 text-center" style={styles}>404 Page Not Found</h1>;
+}
+
+export default NotFoundPage;
