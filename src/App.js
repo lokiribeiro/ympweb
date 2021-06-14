e9581fea8e6a349a5a9a8acd1755c64405f24f9f
Olain <olain@ymp.ltd>
2021-06-14 19:19:00 +0800

    change description in search results

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        <title>Yacht Maintenance Program | Yacht Maintenance Software | YMP</title>
-        <meta charset="UTF-8" />
-        <meta name="description" content="YMP is a cloud-based yacht maintenance program with yacht crew members and management companies in mind" />
-        <meta name="keywords" content="Yacht Maintenance Program, Yacht Maintenance Software, Best Boat Maintenance App, Yacht Management App, Yacht Maintenance App, Yacht Management Software" />
+        <title>Yacht Maintenance Program | YMP</title>
       </Helmet>
       <Main />
     </div>
